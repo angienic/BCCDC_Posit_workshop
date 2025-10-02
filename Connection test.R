aa3132503d58f8084a3eea96e0902e80e1bfa9be
angienic <ajnstella@yahoo.com>
2025-10-02 18:52:49 +0000

    test

---
+++ Connection test.R
@@ -0,0 +1,7 @@
+# Notes from Posit Git workshop 
+
+library(usethis)
+use_git_config(user.name = "angienic", user.email = "ajnstella@yahoo.com")
+usethis::create_github_token()
+credentials::set_github_pat(FALSE)
+
